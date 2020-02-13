d768ce2f1cdb8b29e4f173156c0377cf10cdc71a
White-b-A <tenkei0423@gmail.com>
2020-02-13 23:59:38 +0900

    add

--- src/pac1/DiaryContent.java
+++ src/pac1/DiaryContent.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/DiaryContent")
 public class DiaryContent extends HttpServlet {
+	
 	public static final String DB_NAME = "webapp2019_sgt2";
 	public static final String HOST_NAME = "10.15.121.37:3306";
 	public static final String USER_NAME = "user_sgt2";
